f6fe5e021f2495dc6faa6f493d1ead7c461ffbf5
Supun Wijesooriya <81010139+supunxiii@users.noreply.github.com>
2023-01-30 20:33:53 +0530

    Add files via upload

---
+++ formymandi.py
@@ -0,0 +1,21 @@
+"""COPYRIGHT (C) 2022 Supun Wijesooriya. All Rights Reserved.
+Developer : supun seth xiii
+Composed on: 10/26/22 at 16:53
+@author Supun Wijesooriya
+@version 1.0 10-26-2022
+"""
+import turtle
+from turtle import *
+turtle.setpos(0, 80)
+turtle.write('i love u my mandi. ♡', font=("Lemon", 13, "normal"), align="center")
+turtle.setpos(0, -10)
+color('#e80af0')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
